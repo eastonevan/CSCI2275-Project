032376f5a8470b9e71895a4262d8ff2c85617f7b
eastonevan <49697008+eastonevan@users.noreply.github.com>
2019-12-02 12:24:10 -0700

    Update VS_Easton_FinalProject.cpp

--- VS_Easton_FinalProject.cpp
+++ VS_Easton_FinalProject.cpp
@@ -1,5 +1,78 @@
 #include<iostream>
+#include<queue>
+#include<stack>
+#include<vector>
+
 using namespace std;
+
+/*===========
+	Product
+  ===========*/
+
+struct Product
+{
+	string name;
+	int price;
+	int quantity;
+};
+
+
+
+
+
+/*===========
+	Business
+  ===========*/
+
+class Business
+{
+
+};
+
+
+
+
+
+/*===========
+   Warehouse
+  ===========*/
+
+class Warehouse
+{
+
+};
+
+
+
+
+/*===========
+	Store
+  ===========*/
+
+class Store
+{
+
+};
+
+
+
+
+
+/*===========
+	Driver
+  ===========*/
+
+class Driver
+{
+
+};
+
+
+
+
+/*===========
+	Main
+  ===========*/
 
 int main()
 {
